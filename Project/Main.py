f9d1272d41b9ab49528cdf032a94a596db3d28c3
Haox09 <haoxuan0930@gmail.com>
2025-03-19 09:32:58 +0100

    game window

--- Project/Main.py
+++ Project/Main.py
@@ -1,5 +1,14 @@
 from Functions import *
 import time
 import random
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((1280, 660))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
 print("hello")
 Hello()
