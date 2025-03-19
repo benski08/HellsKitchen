904c417adca431404d9279421aa287f9e00716ba
Haox09 <haoxuan0930@gmail.com>
2025-03-19 14:06:34 +0100

    tab title and window size change

--- Project/Main.py
+++ Project/Main.py
@@ -4,7 +4,8 @@ import random
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1024, 1024))
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Hells_Kitchen')
 running = True
 while running:
     for event in pygame.event.get():
