27c123802a29134b12df7457030a6c02b69b6f2d
benski08 <ben.m.dzikowski@gmail.com>
2025-03-20 12:51:32 +0100

    .

--- Project/Main.py
+++ Project/Main.py
@@ -2,17 +2,23 @@ from Functions import *
 import time, random, sys, pygame
 from images import *
 
+configuration
 pygame.init()
 screen = pygame.display.set_mode((900, 600))
 pygame.display.set_caption('Hells_Kitchen')
 running = True
 bg = pygame.image.load("images/bg.png")
+gameOver = False
+font = pygame.font.SysFont('Arial', 40)
+objects = []
+
+
 while running:
+    screen.blit(bg, (0, 0))
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(bg, (0,0))
-    pygame.display.update()
 pygame.quit()
 print("hello")
 Hello()
