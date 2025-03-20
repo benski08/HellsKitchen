8ddcf7fd62befaa9e85e258b231777ca1a34dc37
benski08 <ben.m.dzikowski@gmail.com>
2025-03-20 15:48:41 +0100

    .

--- Project/Functions.py
+++ Project/Functions.py
@@ -1,3 +1,6 @@
+import random
+import
+
 def Hello():
     print("Hello World")
     return
@@ -5,3 +8,12 @@ def gameStart():
     return
 def gameOver():
     return
+def game_to_menu():
+    return
+def cooking_pot():
+    return
+
+def randomKey():
+    index = random.randint(0,25)
+    return key_list[index]
+
--- Project/Main.py
+++ Project/Main.py
@@ -8,7 +8,8 @@ pygame.init()
 
 screen = pygame.display.set_mode((900, 600))
 pygame.display.set_caption('Hells_Kitchen')
-bg = pygame.image.load("images/bg_title.png")
+title_bg = pygame.image.load("images/title_bg.jpg")
+game_bg = pygame.image.load("images/game_bg.png")
 start_game = False
 gameOver = False
 running = True
@@ -25,10 +26,18 @@ button_width, button_height = 200, 80
 button_x, button_y = (WIDTH - button_width) // 2, (HEIGHT - button_height) // 2 + 200
 button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
 
+#keystroke definitions
+global key_list = ["K_a", "K_b", "K_c", "K_d","K_e", "K_f", "K_g", "K_h","K_i", "K_j", "K_k", "K_l" ]
+global used_list = []
+
+cooking_pot_key = randomKey()
+tea_pot_key = randomKey()
+trash_can_key = randomKey()
+
 
 #game loop
 while running:
-    screen.blit(bg, (0, 0))
+    screen.blit(title_bg, (0, 0))
     pygame.draw.rect(screen, GRAY, button_rect)
     text_surface = font.render("START", True, BLACK)
     text_rect = text_surface.get_rect(center=button_rect.center)
@@ -39,14 +48,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
+
             if button_rect.collidepoint(event.pos):
+                screen.blit(game_bg, (0,0))
                 start_game = True
+
 
     while start_game: #once start button is pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start_game = False
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.
+
         pygame.display.update()
 
 pygame.quit()
