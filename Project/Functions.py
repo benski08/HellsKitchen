0469cf8afff4c7956c66cfbd15e7bb902d398efd
benski08 <ben.m.dzikowski@gmail.com>
2025-03-20 11:09:37 +0100

    .

--- Project/Functions.py
+++ Project/Functions.py
@@ -1,3 +1,7 @@
 def Hello():
     print("Hello World")
     return
+def gameStart():
+    return
+def gameOver():
+    return
--- Project/Main.py
+++ Project/Main.py
@@ -1,16 +1,18 @@
 from Functions import *
-import time
-import random
-import pygame
+import time, random, sys, pygame
+from images import *
 
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((900, 600))
 pygame.display.set_caption('Hells_Kitchen')
 running = True
+bg = pygame.image.load("images/bg.png")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.blit(bg, (0,0))
+    pygame.display.update()
 pygame.quit()
 print("hello")
 Hello()
