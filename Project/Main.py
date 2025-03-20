5f1484ddc1cab2b38e0ba3f19d3af40eddfe359a
benski08 <ben.m.dzikowski@gmail.com>
2025-03-20 13:15:59 +0100

    Game loop created

--- Project/Main.py
+++ Project/Main.py
@@ -1,26 +1,34 @@
+#imports
 from Functions import *
 import time, random, sys, pygame
 from images import *
 
-configuration
+#config
 pygame.init()
 screen = pygame.display.set_mode((900, 600))
 pygame.display.set_caption('Hells_Kitchen')
 running = True
 bg = pygame.image.load("images/bg.png")
+start_game = False
 gameOver = False
 font = pygame.font.SysFont('Arial', 40)
 objects = []
 
 
+#game loop
 while running:
     screen.blit(bg, (0, 0))
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    while start_game: #once start button is pressed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.update()
+
 pygame.quit()
-print("hello")
-Hello()
+
 
 
