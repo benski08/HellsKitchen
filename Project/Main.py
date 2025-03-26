363ac2c2e33ddda1dba4d18c5870287ecb2c8862
benski08 <ben.m.dzikowski@gmail.com>
2025-03-26 09:17:38 +0100

    .

--- Project/Main.py
+++ Project/Main.py
@@ -3,6 +3,8 @@ from pygame import MOUSEBUTTONDOWN
 
 from Functions import *
 import time, random, sys, pygame
+from Tasks import *
+
 
 #config
 pygame.init()
@@ -24,6 +26,7 @@ running = True
 GRAY = (180,180,180)
 BLACK = (0,0,0)
 GREEN = (0, 255, 0)
+WHITE = (255,255,255)
 WIDTH = screen.get_width()
 HEIGHT = screen.get_height()
 FRAMERATE = 30
@@ -38,13 +41,26 @@ button_rect = pygame.Rect(menu_button_x, menu_button_y, menu_button_width, menu_
 text_surface = menu_font.render("START", True, BLACK)
 text_rect = text_surface.get_rect(center=button_rect.center)
 
+#HIGHSCORETEXT
+score_font = pygame.font.SysFont("Arial", 40)
+score_width = 100
+score_height = 30
+score_x = WIDTH - score_width //2
+score_y = HEIGHT - score_height//2
+
+def scoreRender(score,x,y,width,height):
+    high_score_surface = score_font.render(f"HIGHSCORE: {score}", True, WHITE)
+    hs_rect = pygame.Rect(x, y, width, height)
+    screen.blit(high_score_surface, hs_rect)
+
 #keystroke definitions
 key_list = ["K_a", "K_b", "K_c", "K_d","K_e", "K_f", "K_g", "K_h","K_i", "K_j", "K_k", "K_l", "K_m", "K_n", "K_o", "K_p", "K_q", "K_r","K_s","K_t", "K_u","K_v","K_w", "K_x", "K_y", "K_z"]
 used_list = ["K_a","K_a","K_a"]
 
 #controlsinfo buttons
 info_font = pygame.font.SysFont("Arial", 30, bold=True)
-info_button_width, info_button_height = 40, 40
+SIDELENGTH = 25
+
 
 # Play Again Button
 play_again_font = pygame.font.SysFont('Arial', 35)
@@ -79,88 +95,25 @@ def gameOver():
                 pass
     return
 
-def randomKey(previous_letter):
-    n = 0
-    for item in used_list:
-        if item == previous_letter:
-            used_list.pop(n)
-            n += 1
-    index = random.randint(0, 25)
-    key = key_list[index]
-    used_list.append(key)
-    return key
-
-class Task:
-    def __init__(self, x,y, progress, key):
-        self.x = x
-        self.y = y
-        self.progress = progress
-        self.key = key
-        return
-
-    def pBarUpdate(self):
-        self.MAXTIME = 5
-        self.MAXWIDTH = 80
-        self.HEIGHT = 20
-        self.WIDTH = self.MAXWIDTH/100 * self.progress
-        self.XDISPLACEMENT = self.WIDTH/2
-        self.p_bar_rect = pygame.Rect(self.x - self.XDISPLACEMENT,self.y, self.WIDTH, self.HEIGHT)
-        self.COLOR = (self.progress/100 * 255, (1-self.progress/100)*255,0)
-        pygame.draw.rect(screen, self.COLOR, self.p_bar_rect)
-        self.progress += 100/(self.MAXTIME*30)
-        return self.progress
-
-    def controlInfo(self,surface, x, y, side_length):
-        self.surface = surface
-        self.side_length = side_length
-        self.x = x
-        self.y = y
-        self.cI_rect = pygame.Rect(x, y, side_length, side_length)
-        self.text_rect = surface.get_rect(center=self.cI_rect.center)
-        pygame.draw.rect(screen, BLACK, self.cI_rect)
-        screen.blit(CP_info_surface, self.text_rect)
-    pass
-class CookingPot(Task):
-    def __init__(self, x, y, progress, key):
-        super().__init__(x, y, progress, key)
-        self.toggle_lid = True
-        self.frame_counter = 0
-    def animate(self):
-        self.min_frames = 5 #in frames
-        self.max_frames = 30 #in frames
-        self.switch_rate = int(self.max_frames - (self.progress / 100) * (self.max_frames - self.min_frames)) #calculate switchrate based on progress
-        print(self.switch_rate)
-        if self.frame_counter == self.switch_rate: #if the framecounter is high enough, it will switch states of lid
-            self.toggle_lid = not self.toggle_lid
-            self.frame_counter = 0
-        if self.toggle_lid == True:
-            screen.blit(cookingpot_lid_right, ((WIDTH - 50) // 2 + 182, (HEIGHT - 50) // 2 - 12))
-        else:
-            screen.blit(cookingpot_lid_left, ((WIDTH - 50) // 2 + 182, (HEIGHT - 50) // 2 - 12))
-        self.frame_counter += 1
-
-
-
 cooking_pot_x, cooking_pot_y = (WIDTH - 50) // 2 + 250, (HEIGHT - 40) // 2
-Cooking_pot = CookingPot(cooking_pot_x, cooking_pot_y, 0, randomKey("K_a"))
-tea_pot_key = randomKey("K_a")
-trash_can_key = randomKey("K_a")
-plates_key = randomKey("K_a")
+Cooking_pot = CooPot(0, "K_a", screen, key_list, used_list, info_font)
+
 print(Cooking_pot.key, used_list)
 
 
 #cooking pot button
 CP_info_surface = info_font.render(Cooking_pot.key[2], True, (255, 255, 255))
-CP_info_button_x, CP_info_button_y = (WIDTH - info_button_width) // 2 + 400, (HEIGHT - info_button_height) // 2 + 250
-
+CP_info_button_x, CP_info_button_y = (WIDTH - SIDELENGTH) // 2 + 400, (HEIGHT - SIDELENGTH) // 2 + 250
+print(CP_info_button_x, CP_info_button_y)
 
 #tea pot button
-TP_info_surface = info_font.render(tea_pot_key[2], True, (255, 255, 255))
-TP_info_button_x, TP_info_button_y = (WIDTH - info_button_width) // 2 + 320, (HEIGHT - info_button_height) // 2 + 250
+#TP_info_surface = info_font.render(tea_pot_key[2], True, (255, 255, 255))
+#TP_info_button_x, TP_info_button_y = (WIDTH - info_button_width) // 2 + 400, (HEIGHT - info_button_height) // 2 + 250
 
 
 #game loop
 while running:
+    score = 0
     screen.blit(title_bg, (0, 0))
     pygame.draw.rect(screen, GRAY, button_rect)
     screen.blit(text_surface, text_rect)
@@ -175,9 +128,9 @@ while running:
                 start_game = True
 
     while start_game: #once start button is pressed
+        screen.blit(game_bg, (0, 0))
         #render buttons
-        CookingPot.controlInfo(Cooking_pot, CP_info_surface, CP_info_button_x, CP_info_button_y, 40)
-        screen.blit(plate, ((WIDTH - info_button_width) // 2 + 320, (HEIGHT - info_button_height) // 2 + 250))
+        CooPot.controlInfo(Cooking_pot, 25)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start_game = False
@@ -185,14 +138,16 @@ while running:
             if event.type == pygame.KEYDOWN:
                 if event.key == getattr(pygame, Cooking_pot.key):
                     print("Cooking pot")
-                    cooking_pot_interact()
-                elif event.key == getattr(pygame, tea_pot_key):
-                    print("Tea pot")
+                    score += Cooking_pot.calculateScore()
+                    Cooking_pot.interact()
+                #elif event.key == getattr(pygame, tea_pot_key):
+                    #print("Tea pot")
         if Cooking_pot.progress >= 100:
             gameOver()
         else:
             Cooking_pot.pBarUpdate()
-            Cooking_pot.animate()
+            Cooking_pot.animate(cookingpot_lid_left, cookingpot_lid_right)
+        scoreRender(score, score_x,score_y, score_width, score_height)
         pygame.display.update()
         clock.tick(FRAMERATE)
 
---
+++ Project/Tasks.py
@@ -0,0 +1,84 @@
+import pygame
+
+class Task:
+    def __init__(self, progress, key, screen, key_list, used_list, info_font):
+        self.font = info_font
+        self.screen = screen
+        self.used_list = used_list
+        self.key_list = key_list
+        self.HEIGHT = screen.get_height()
+        self.WIDTH = screen.get_width()
+        self.progress = progress
+        self.key = key
+        self.BLACK = (0,0,0)
+        return
+    def randomKey(self, previous_letter):
+        import random
+        n = 0
+        for item in self.used_list:
+            if item == previous_letter:
+                self.used_list.pop(n)
+                n += 1
+        index = random.randint(0, 25)
+        self.key = self.key_list[index]
+        self.used_list.append(self.key)
+        return self.key, self.used_list
+    def calculateScore(self):
+        self.MINSCORE = 0
+        self.MAXSCORE = 100
+        self.score = (self.MAXSCORE/100) * self.progress
+        return self.score
+
+class CooPot(Task):
+    import pygame
+    def __init__(self, progress, key, screen, key_list, used_list, info_font):
+        super().__init__(progress, key, screen, key_list, used_list, info_font)
+        self.toggle_lid = True
+        self.frame_counter = 0
+        self.pbarx = (self.screen.get_width() - 80) // 2 + 241
+        self.pbary = (self.screen.get_height() - 70)// 2 - 37
+    def animate(self, state_1, state_2):
+        #pot lid animation
+        self.state_1 = state_1
+        self.state_2  = state_2
+        self.rlid_x = (self.screen.get_width() - 80)// 2 + 195
+        self.rlid_y = (self.screen.get_height() - 70)// 2
+        self.llid_x = (self.screen.get_width() - 80) // 2 + 195
+        self.llid_y = (self.screen.get_height() - 61) // 2 - 10
+        self.min_frames = 1 #in frames
+        self.max_frames = 30 #in frames
+        self.switch_rate = int(self.max_frames - (self.progress / 100) * (self.max_frames - self.min_frames)) #calculate switchrate based on progress
+        if self.state_1 is None or self.state_2 is None:
+            print("Error: Images not loaded properly")
+        if self.frame_counter >= self.switch_rate: #if the framecounter is high enough, it will switch states of lid
+            self.toggle_lid = not self.toggle_lid
+            self.frame_counter = 0
+        if self.toggle_lid == True:
+            self.screen.blit(self.state_2, (self.rlid_x, self.rlid_y))
+        else:
+            self.screen.blit(self.state_1, (self.llid_x, self.llid_y))
+        self.frame_counter += 1
+        self.pygame.display.update()
+    def interact(self):
+        self.progress = 0
+        self.randomKey(self.key)
+        #score
+    def pBarUpdate(self):
+        self.MAXTIME = 5
+        self.pbarMAXWIDTH = 80
+        self.pbarHEIGHT = 10
+        self.pbarWIDTH = self.pbarMAXWIDTH/100 * self.progress
+        self.XDISPLACEMENT = self.pbarWIDTH/2
+        self.p_bar_rect = pygame.Rect(self.pbarx - self.XDISPLACEMENT,self.pbary, self.pbarWIDTH, self.pbarHEIGHT)
+        self.COLOR = (self.progress/100 * 255, (1-self.progress/100)*255,0)
+        pygame.draw.rect(self.screen, self.COLOR, self.p_bar_rect)
+        self.progress += 100/(self.MAXTIME*30)
+        return self.progress
+    def controlInfo(self, side_length):
+        self.side_length = side_length
+        self.surface = self.font.render(self.key[2], True, (255, 255, 255))
+        x, y = self.pbarx - (side_length//2),self.pbary + 15
+        self.cI_rect = pygame.Rect(x, y, side_length, side_length)
+        self.text_rect = self.surface.get_rect(center=self.cI_rect.center)
+        pygame.draw.rect(self.screen, self.BLACK, self.cI_rect)
+        self.screen.blit(self.surface, self.text_rect)
